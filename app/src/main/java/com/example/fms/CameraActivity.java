371214c8d2b70513abb182a79713ac5f45146e42
이현호 <lash270@gmail.com>
2021-06-01 21:25:12 +0900

    camera -> notification no using FB

---
+++ app/src/main/java/com/example/fms/CameraActivity.java
@@ -0,0 +1,273 @@
+package com.example.fms;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class CameraActivity extends AppCompatActivity {
+
+    private static final String TAG = "KNLBDC";
+    private static Bitmap Tokenoriginal;
+    private static File pushtempFile;
+
+    private Boolean isPermission = true;
+
+    private static final int PICK_FROM_ALBUM = 1;
+    private static final int PICK_FROM_CAMERA = 2;
+
+    private Boolean isCamera = false;
+    private File tempFile;
+    Button upload;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_image);
+
+        upload = findViewById(R.id.upload);
+        upload.setVisibility(View.INVISIBLE);
+
+
+        tedPermission();
+
+        findViewById(R.id.btnGallery).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 권한 허용에 동의하지 않았을 경우 토스트를 띄웁니다.
+                if(isPermission) goToAlbum();
+                else Toast.makeText(view.getContext(), getResources().getString(R.string.permission_2), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        findViewById(R.id.btnCamera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 권한 허용에 동의하지 않았을 경우 토스트를 띄웁니다.
+                if(isPermission)  takePhoto();
+                else Toast.makeText(view.getContext(), getResources().getString(R.string.permission_2), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != Activity.RESULT_OK) {
+            Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT).show();
+
+            if (tempFile != null) {
+                if (tempFile.exists()) {
+                    if (tempFile.delete()) {
+                        Log.e(TAG, tempFile.getAbsolutePath() + " 삭제 성공");
+                        tempFile = null;
+                    }
+                }
+            }
+
+            return;
+        }
+
+        if (requestCode == PICK_FROM_ALBUM) {
+
+            Uri photoUri = data.getData();
+            Log.d(TAG, "PICK_FROM_ALBUM photoUri : " + photoUri);
+
+            Cursor cursor = null;
+
+            try {
+
+                /*
+                 *  Uri 스키마를
+                 *  content:/// 에서 file:/// 로  변경한다.
+                 */
+                String[] proj = {MediaStore.Images.Media.DATA};
+
+                assert photoUri != null;
+                cursor = getContentResolver().query(photoUri, proj, null, null, null);
+
+                assert cursor != null;
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+                cursor.moveToFirst();
+
+                tempFile = new File(cursor.getString(column_index));
+
+                Log.d(TAG, "tempFile Uri : " + Uri.fromFile(tempFile));
+
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+
+            setImage();
+
+        } else if (requestCode == PICK_FROM_CAMERA) {
+            setImage();
+
+
+        }
+    }
+
+    /**
+     *  앨범에서 이미지 가져오기
+     */
+    private void goToAlbum() {
+        isCamera = false;
+
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+
+    /**
+     *  카메라를 이미지 가져오기
+     */
+    private void takePhoto() {
+        isCamera = true;
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        try {
+            tempFile = createImageFile();
+        } catch (IOException e) {
+            Toast.makeText(this, "이미지 처리 오류! 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+            finish();
+            e.printStackTrace();
+        }
+        if (tempFile != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+
+                Uri photoUri = FileProvider.getUriForFile(this,
+                        "com.example.fms", tempFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(intent, PICK_FROM_CAMERA);
+
+            } else {
+
+                Uri photoUri = Uri.fromFile(tempFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(intent, PICK_FROM_CAMERA);
+
+            }
+        }
+    }
+
+    /**
+     *  폴더 및 파일 만들기
+     */
+    private File createImageFile() throws IOException {
+
+        // 이미지 파일 이름
+        String timeStamp = new SimpleDateFormat("HHmmss").format(new Date());
+        String imageFileName = "KNLBDC_" + timeStamp + "_";
+
+        // 이미지가 저장될 파일 주소
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/KNLBDC/");
+        if (!storageDir.exists()) storageDir.mkdirs();
+
+        // 빈 파일 생성
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        Log.d(TAG, "createImageFile : " + image.getAbsolutePath());
+
+        return image;
+    }
+
+    /**
+     *  tempFile 을 bitmap 으로 변환 후 ImageView 에 설정한다.
+     */
+    private void setImage() {
+        ImageView imageView = findViewById(R.id.imageView);
+
+        ImageResizeUtils.resizeFile(tempFile, tempFile, 1280, isCamera);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        Bitmap original = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
+        Log.d(TAG, "setImage : " + tempFile.getAbsolutePath());
+
+        imageView.setImageBitmap(original);
+        upload.setVisibility(View.VISIBLE);
+
+        setTempFile(tempFile);
+
+        findViewById(R.id.upload).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Token.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    public File getTempFile() {
+        return pushtempFile;
+    }
+
+    public void setTempFile(File tempFile) {
+        CameraActivity.pushtempFile = tempFile;
+    }
+
+    /**
+     *  권한 설정
+     */
+    private void tedPermission() {
+
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                // 권한 요청 성공
+                isPermission = true;
+
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                // 권한 요청 실패
+                isPermission = false;
+
+            }
+        };
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage(getResources().getString(R.string.permission_2))
+                .setDeniedMessage(getResources().getString(R.string.permission_1))
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .check();
+    }
+
+}
--- app/src/main/java/com/example/fms/Token.java
+++ app/src/main/java/com/example/fms/Token.java
@@ -3,48 +3,41 @@ package com.example.fms;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import com.example.fms.CameraActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.messaging.FirebaseMessaging;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
 
 public class Token extends AppCompatActivity {
     Button btn_CreatePush;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_push);
+
 
     btn_CreatePush = findViewById(R.id.btn_CreatePush);
     btn_CreatePush.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            NotificationManager notification = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+            //notification manager 생성!
+           NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
 
             String id = "message_push";
             CharSequence name = "message push";
@@ -58,34 +51,62 @@ public class Token extends AppCompatActivity {
 
             push_channel.setDescription(description);
 
-            notification.createNotificationChannel(push_channel); //채널을 등록해줍니다!
+            notificationManager.createNotificationChannel(push_channel); //채널을 등록해줍니다!
 
             int notifyID = 1; //알림의 ID
             String CHANNEL_ID = "message_push";
 
-            Drawable drawable = getResources().getDrawable(R.drawable.bell);
-            Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
 
-            //알림 채널에 push라는 알림을 만들어 연결합니다!
-            Notification push = new Notification.Builder(Token.this, CHANNEL_ID)
+            CameraActivity CA = new CameraActivity();
+            File Pushtemp=CA.getTempFile();
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            Bitmap bitmap = BitmapFactory.decodeFile(Pushtemp.getAbsolutePath(), options);
+            //ImageView ImageViewPush = (ImageView)findViewById(R.id.imageViewPush);
+            //ImageViewPush.setImageBitmap(bitmap);
 
+            Intent intent = new Intent(Token.this, Token.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                    | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(Token.this, 0 /* Request code */, intent,
+                    PendingIntent.FLAG_ONE_SHOT);
+
+
+           //알림 채널에 push라는 알림을 만들어 연결합니다!
+
+            Notification push = new NotificationCompat.Builder(Token.this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.bell)
                     .setContentTitle("New Message")
                     .setContentText("장지민님으로부터 사진 도착!!") //데이터베이스에 저장된 사용자의 이름과 카테고리로 수정 필요
-                    .setSmallIcon(R.drawable.bell)
                     .setChannelId(CHANNEL_ID)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+                    .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap))
                     .build();
-            NotificationCompat.BigPictureStyle style = new NotificationCompat.BigPictureStyle();
-            style.bigPicture(bitmap).setBigContentTitle("dd");
 
-
-            notification.notify(notifyID, push);
+            notificationManager.notify(notifyID, push);
         }
     });
     }
-
-
-
-
-
-
 }
+
+////알림 채널에 push라는 알림을 만들어 연결합니다!
+//            /*Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//            Notification.Builder builder = new Notification.Builder(Token.this, CHANNEL_ID)
+//                    .setSmallIcon(R.drawable.bell).setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher) )
+//                    .setContentTitle("title")
+//                    .setContentText("두 손가락을 이용해 아래로 당겨 주세요▼ ")
+//                    .setAutoCancel(true)
+//                    .setSound(defaultSoundUri)
+//                    .setContentIntent(pendingIntent);
+//            NotificationCompat.BigTextStyle style = new NotificationCompat.BigTextStyle();
+//            style.bigText("contedddddddddddddddddddddddnt").setBigContentTitle("title");
+//
+//
+//            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(Token.this);
+//            notificationManager.notify(notifyID, builder.build());*/
+//
+//            /*NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+//            builder.setContentIntent(notifyPendingIntent);
+//
+//            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+//            notificationManager.notify(NOTIFICATION_ID, builder.build());*/
